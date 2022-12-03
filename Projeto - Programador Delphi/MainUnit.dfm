83cf799e207be9d5abae8a58b9e439312a1482b3
FelipeMartere <felipemartere@hotmail.com>
2022-12-03 11:19:23 -0300

    Escolha da categoria - OK
    Valores sendo recebidos e exibidos na ComboBox ao carregar o MainForm, em seu OnShow().

--- Projeto - Programador Delphi/MainUnit.dfm
+++ Projeto - Programador Delphi/MainUnit.dfm
@@ -1,6 +1,6 @@
 object MainForm: TMainForm
-  Left = 238
-  Top = 246
+  Left = 2057
+  Top = 222
   Width = 1305
   Height = 675
   Caption = 'Tabela de Aplica'#231#227'o Rasther'
@@ -11,6 +11,55 @@ object MainForm: TMainForm
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 13
+  object lbl1: TLabel
+    Left = 256
+    Top = 88
+    Width = 236
+    Height = 24
+    Caption = 'Escolha uma das categorias'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -19
+    Font.Name = 'MS Sans Serif'
+    Font.Style = []
+    ParentFont = False
+  end
+  object btn1: TButton
+    Left = 256
+    Top = 184
+    Width = 75
+    Height = 25
+    Caption = 'Enviar GET'
+    TabOrder = 0
+    OnClick = btn1Click
+  end
+  object cbb1: TComboBox
+    Left = 256
+    Top = 128
+    Width = 225
+    Height = 21
+    ItemHeight = 13
+    MaxLength = 50
+    TabOrder = 1
+  end
+  object idhtp1: TIdHTTP
+    MaxLineAction = maException
+    ReadTimeout = 0
+    AllowCookies = True
+    ProxyParams.BasicAuthentication = False
+    ProxyParams.ProxyPort = 0
+    Request.ContentLength = -1
+    Request.ContentRangeEnd = 0
+    Request.ContentRangeStart = 0
+    Request.ContentType = 'text/html'
+    Request.Accept = 'text/html, */*'
+    Request.BasicAuthentication = False
+    Request.UserAgent = 'Mozilla/3.0 (compatible; Indy Library)'
+    HTTPOptions = [hoForceEncodeParams]
+    Left = 16
+    Top = 8
+  end
 end
--- Projeto - Programador Delphi/MainUnit.pas
+++ Projeto - Programador Delphi/MainUnit.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient,
+  IdHTTP, StdCtrls, Menus, uLkJSON;
 
 type
   TMainForm = class(TForm)
+    btn1: TButton;
+    idhtp1: TIdHTTP;
+    cbb1: TComboBox;
+    lbl1: TLabel;
+    procedure btn1Click(Sender: TObject);
+    procedure GETCategoria();
+    procedure FormShow(Sender: TObject);
+
   private
     { Private declarations }
   public
@@ -20,5 +29,45 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TMainForm.btn1Click(Sender: TObject);
+var JSONRest : String;
+    objJSON, Item: TlkJSONbase;
+    i : Integer;
+begin
+  JSONRest := idhtp1.Get('http://service.tecnomotor.com.br/iRasther/tipo?pm.platform=1&pm.version=23');
+  objJSON := TlkJSON.ParseText(JSONRest);
+
+  for i :=0 to objJSON.Count-1 do
+  begin
+    Item := objJSON.Child[i];
+    cbb1.Items.Add(Item.Value);
+  end;
+
+end;
+
+procedure TMainForm.GETCategoria();
+var JSONRest : String;
+    objJSON, Items, Item: TlkJSONbase;
+    i : Integer;
+begin
+
+  JSONRest := idhtp1.Get('http://service.tecnomotor.com.br/iRasther/tipo?pm.platform=1&pm.version=23');
+  objJSON := TlkJSON.ParseText(JSONRest);
+
+  for i :=0 to objJSON.Count-1 do
+  begin
+    Item := objJSON.Child[0];
+    Items := objJSON.Child[i];
+    cbb1.Items.Add(Items.Value);
+    cbb1.Text := VarToStr(Item.Value);
+  end;
+
+end;
+
+procedure TMainForm.FormShow(Sender: TObject);
+begin
+  GETCategoria();
+end;
 
 end.
